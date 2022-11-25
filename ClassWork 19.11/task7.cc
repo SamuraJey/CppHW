33445cd1972b4806166c6adb272cd7a69e1f46a3
SamuraJ <pilot242@bk.ru>
2022-11-25 23:25:06 +0500

    task7 ClassWork done

---
+++ ClassWork 19.11/task7.cc
@@ -0,0 +1,43 @@
+#include <math.h>
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    double x, y, n, pp ,pp1,pp2; // pp = answ
+
+    double people_quantity, orig_raiting, target_raiting, answ, people_min, people_max, people;
+    int sum_of_reviews;
+
+    cin >> orig_raiting >> target_raiting >> people_quantity; 
+
+    if (target_raiting > orig_raiting)
+    {
+        cout << "Impossible" << endl;
+        return 0;
+    }
+     
+    sum_of_reviews = ceil(orig_raiting * people_quantity);
+
+    target_raiting = target_raiting + 0.049; 
+    answ = (sum_of_reviews - target_raiting * people_quantity) / (target_raiting - 1);
+    people_min = round((round(answ) + sum_of_reviews) / (people_quantity + round(answ)) * 100) / 100;
+    people_max = round((round(answ) + 1 + sum_of_reviews) / (people_quantity + round(answ) + 1) * 100) / 100;
+
+    if (people_min >= target_raiting-0.049 && people_min <= target_raiting)
+    {
+        cout << int(answ) << endl;
+    }
+    else
+    {
+    if (people_max >= target_raiting - 0.049 && people_max <= target_raiting)
+    {
+        cout << int(answ + 1) << endl;
+    }
+    else
+    {
+        cout << "Impossible" << endl;
+    }
+    }
+}
