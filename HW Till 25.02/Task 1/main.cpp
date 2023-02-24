3fdefa56309d77dad29b8abfc53f33706dbd5697
SamuraJ <cergeypro703@gmail.com>
2023-02-25 01:06:17 +0500

    task 1 done?

---
+++ HW Till 25.02/Task 1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "task1_header.h"
+
+using namespace std;
+
+int main()
+{
+    cout << global_value << endl;
+}
---
+++ HW Till 25.02/Task 1/task1_header.h
@@ -0,0 +1,10 @@
+//
+// Created by samuraj on 25.02.23.
+//
+
+#ifndef MULTIFILE_HW_TASK1_HEADER_H
+#define MULTIFILE_HW_TASK1_HEADER_H
+
+#endif //MULTIFILE_HW_TASK1_HEADER_H
+
+extern int global_value;
