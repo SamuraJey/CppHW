1c4b9f8c9417970e36887507fffed7df8962fbc2
SamuraJ <pilot242@bk.ru>
2023-04-03 20:14:14 +0500

    task 666 Memory limit exceeded

---
+++ HW till 08.04.2023/task_666_Abracadabra.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int n = 0;
+    cin >> n;
+    string sequence = "a"; // начальная строка
+    for (int i = 1; i <= 25; i++) // проходим по всем буквам алфавита (от b до z)
+    {
+        sequence = char('a' + i) + sequence + sequence; // добавляем новую букву слева и удваиваем строку
+    }
+    cout << sequence[n - 1] << endl;
+    return 0;
+}
