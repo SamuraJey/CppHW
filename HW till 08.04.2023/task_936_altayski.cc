b1af1c0aa61d39d8a0c1c157bea7e3c7b499e405
SamuraJ <pilot242@bk.ru>
2023-04-08 00:25:30 +0500

    936

--- HW till 08.04.2023/task_936_altayski.cc
+++ HW till 08.04.2023/task_936_altayski.cc
@@ -1,23 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
+
 using namespace std;
 
-int main() {
-    int x, y, n;
-    double r;
-    cin >> x >> y >> r >> n;
-
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        int xi, yi;
-        cin >> xi >> yi;
-
-        double distance = sqrt((x - xi) * (x - xi) + (y - yi) * (y - yi));
-        if (distance <= r && abs(xi - x) <= sqrt(r * r - (y - yi) * (y - yi))) {
-            count++;
+int max_visible_points(double x, double y, double r, vector<pair<double, double>>& points) {
+    int max_count = 0;
+    for (int i = 0; i < points.size(); i++) {
+        double dist = sqrt(pow(points[i].first - x, 2) + pow(points[i].second - y, 2));
+        if (dist <= r) {
+            int count = 0;
+            for (int j = 0; j < points.size(); j++) {
+                if (j != i) {
+                    double angle1 = atan2(points[j].second - y, points[j].first - x);
+                    double angle2 = atan2(points[i].second - y, points[i].first - x);
+                    double angle = angle1 - angle2;
+                    if (angle < 0) angle += 2 * M_PI;
+                    if (angle <= M_PI && dist >= sqrt(pow(points[j].first - x, 2) + pow(points[j].second - y, 2))) {
+                        count++;
+                    }
+                }
+            }
+            if (count > max_count) {
+                max_count = count;
+            }
         }
     }
+    return max_count + 1;
+}
 
-    cout << count << endl;
+int main() 
+{
+    double x, y, r;
+    int n;
+    cin >> x >> y >> r >> n;
+    vector<pair<double, double>> points(n);
+    for (int i = 0; i < n; i++) {
+        cin >> points[i].first >> points[i].second;
+    }
+    int max_points = max_visible_points(x, y, r, points);
+    cout << max_points << endl;
     return 0;
 }
