666ba792d7d09f08af2f9af8e8649841b6651879
SamuraJ <pilot242@bk.ru>
2023-04-14 22:03:05 +0500

    334

---
+++ HW till 15.04.2023/task_334_kitayskie_chasi.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> seconds(n);
+
+    for (int i = 0; i < n; i++) {
+        int hours, minutes, secs;
+        scanf("%d:%d:%d", &hours, &minutes, &secs);
+        int timeInSeconds = hours * 3600 + minutes * 60 + secs;
+        seconds[i] = timeInSeconds;
+    }
+
+    int bestTime = seconds[0];
+    int minTimeDifference = abs(seconds[0] - (2 * 3600 * n));
+
+    for (int i = 0; i < n; i++) {
+        int timeDifference = abs((2 * 3600 * n) - 2 * seconds[i]);
+        if (timeDifference < minTimeDifference) {
+            minTimeDifference = timeDifference;
+            bestTime = seconds[i];
+        }
+    }
+
+    int hours = bestTime / 3600;
+    int minutes = (bestTime % 3600) / 60;
+    int seconds1 = bestTime % 60;
+
+    if (hours == 0) {
+        hours = 12;
+    }
+
+    if (hours > 12) {
+        hours -= 12;
+    }
+
+    if (hours < 10) 
+    {
+        printf("%1d:%02d:%02d\n", hours, minutes, seconds1);
+    } 
+    else 
+    {
+        printf("%02d:%02d:%02d\n", hours, minutes, seconds1);
+    }
+
+    return 0;
+}
