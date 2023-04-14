87dfe9e197312cc496d2b2152faba2ad4071a892
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-04-14 22:30:34 +0500

    M_PI fix

--- HW till 15.04.2023/task_559_sosiska_v_teste.cc
+++ HW till 15.04.2023/task_559_sosiska_v_teste.cc
@@ -2,15 +2,13 @@
 #include <cmath>
 #include <iomanip>
 
-#define pi M_PI // определение числа пи
-
 using namespace std;
 
 int main()
 {
     int n;
-    double diameter, radius, area, left, right, middle, midArea, theta;
-
+    double diameter, radius, area, left, right, middle, midArea, theta, pi;
+    pi = 3.14159265358979323846; // ОНО НЕ ПРИНИМАЕТ M_PI из cmath аааааа
     cin >> diameter >> radius >> n;
 
     for (int i = 1; i < n; i++)
@@ -35,7 +33,7 @@ int main()
                 else
                 {
                     theta = 2 * acos((radius - (2 * radius + diameter - middle)) / radius); // вычисление угла
-                    midArea = (pi * radius * radius + 2 * radius * diameter) - radius*radius/2 * (theta - sin(theta)); // вычисление площади сектора и двух треугольников
+                    midArea = (pi * radius * radius + 2 * radius * diameter) - radius * radius/2 * (theta - sin(theta)); // вычисление площади сектора и двух треугольников
                 }
             }
 
