8abd668f2909545515ff900cd7105097d11743f5
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-04-22 14:25:35 +0500

    1709 pinguin avia

--- ClassWork 22.04.2023/timus_1709_Pingui-Avia.cc
+++ ClassWork 22.04.2023/timus_1709_Pingui-Avia.cc
@@ -2,52 +2,52 @@
 
 using namespace std;
 
-int get_parent(int idx, vector<int> &parent) // Функция поиска родителя
+int getParent(int idx, vector<int> &parent) // Функция поиска родителя
 {
     if (idx == parent[idx]) // Если индекс равен родителю, то возвращаем индекс
     {
         return idx;
     }
-    return parent[idx] = get_parent(parent[idx], parent); // Иначе возвращаем родителя родителя рекурсивно идя до корня
+    return parent[idx] = getParent(parent[idx], parent); // Иначе возвращаем родителя родителя рекурсивно идя до корня
 }
 
-void merge_set(int a, int b, vector<int> &parent, vector<int> &rnk) // Функция объединения двух множеств
+void mergeSet(int a, int b, vector<int> &parent, vector<int> &rank) // Функция объединения двух множеств
 {
-    a = get_parent(a, parent); // Находим родителей
-    b = get_parent(b, parent);
+    a = getParent(a, parent); // Находим родителей
+    b = getParent(b, parent);
     if (a != b) // Если они не равны, то объединяем
     {
-        if (rnk[a] < rnk[b]) // Если ранг родителя меньше ранга ребенка, то меняем их местами
+        if (rank[a] < rank[b]) // Если ранг родителя меньше ранга ребенка, то меняем их местами
         {
             swap(a, b);
         }
         parent[b] = a; // Присваиваем родителю ребенка
-        if (rnk[a] == rnk[b]) // Если ранги равны, то увеличиваем ранг родителя
+        if (rank[a] == rank[b]) // Если ранги равны, то увеличиваем ранг родителя
         {
-            rnk[a]++; 
+            rank[a]++; 
         }
     }
 }
 
-void initialize(int n, vector<int> &parent, vector<int> &rnk) // Функция инициализации массивов родителей и рангов
+void initArr(int n, vector<int> &parent, vector<int> &rank) // Функция инициализации массивов родителей и рангов
 {
     parent.resize(n + 1); // Размер массива родителей равен количеству вершин + 1
-    rnk.resize(n + 1); // Размер массива рангов равен количеству вершин + 1
+    rank.resize(n + 1); // Размер массива рангов равен количеству вершин + 1
     for (int i = 1; i <= n; i++) // Присваиваем родителям их индексы
     {
         parent[i] = i; // Родитель вершины i равен i
-        rnk[i] = 0; // Ранг вершины i равен 0
+        rank[i] = 0; // Ранг вершины i равен 0
     }
 }
 
 int main()
 {
     vector <int> parent;
-    vector <int> rnk;
+    vector <int> rank;
     int n, d, a;
     cin >> n >> d >> a;
     vector <string> inpArr(n); // Массив строк, в котором хранятся ребра графа
-    initialize(n, parent, rnk); // Инициализируем массивы родителей и рангов
+    initArr(n, parent, rank); // Инициализируем массивы родителей и рангов
 
     for (int i = 0; i < n; i++) // Вводим граф
     {
@@ -70,11 +70,11 @@ int main()
             // Если ребро не было добавлено в ответ и ребро не было добавлено в ответ в обратном порядке
             if (inpArr[i][j] == '1' and added.find({i, j}) == added.end() and added.find({j, i}) == added.end()) 
             { // Добавляем ребро в ответ
-                added[ {i, j}] = true;
+                added[{i, j}] = true;
                 // Если родители вершин не равны, то объединяем их
-                if (get_parent(i + 1, parent) != get_parent(j + 1, parent))
+                if (getParent(i + 1, parent) != getParent(j + 1, parent))
                 {
-                    merge_set(i + 1, j + 1, parent, rnk);
+                    mergeSet(i + 1, j + 1, parent, rank);
                 }
                 else
                 { // Иначе добавляем стоимость удаления ребра
@@ -99,7 +99,7 @@ int main()
     */
     for (int i = 1; i <= n; i++)
     {
-        parentsOf[get_parent(i, parent)] = 1; // Присваиваем родителям 1
+        parentsOf[getParent(i, parent)] = 1; // Присваиваем родителям 1
     }
     for (int i = 1; i <= n; i++)
     {
