f62840322fa545d5dcb4fac6c9762085d4feb711
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-04-28 20:48:51 +0500

    1301 upd

--- HW till 29.04.2023/task_1301_kubik-v-labirinte.cc
+++ HW till 29.04.2023/task_1301_kubik-v-labirinte.cc
@@ -1,13 +1,13 @@
-/* 1301. Cube in Labyrinth - http://acm.timus.ru/problem.aspx?num=1301
- *
- * Strategy:
- * Depth-first search through the maze while recording the shortest path to each position for
- * each configuration of the cube, terminating any branch whenever a shorter path than the
- * current was reached for the position and configuration of the branch.
- *
- * Performance:
- * O(XY), runs the tests in 0.001s using 404KB memory.
- */
+/*
+1301. Куб в лабиринте - http://acm.timus.ru/problem.aspx?num=1301
+
+Программа создает лабиринт в форме куба и находит самый короткий путь от одной точки к другой.
+Она использует графы и поиск в глубину (DFS) для обхода лабиринта.
+Лабиринт представлен в виде матрицы видимости граней куба.
+Программа проверяет возможность перемещения из одной точки в другую и определяет конфигурацию куба после поворота и переворота грани.
+Она также подсчитывает самую короткую длину пути с помощью матрицы с самой короткой длиной пути.
+В конце программа выводит длину самого короткого пути или сообщение "No solution", если путь не найден.
+*/
 
 #include <algorithm>
 #include <string>
@@ -20,121 +20,123 @@
 using namespace std;
 
 // создаем глобальные переменные туточки
-int N[255]; // Соседний край индексного ребра
-unsigned int A[255][11][11]; // Матриа с самой короткой длиной пути
-bool V[11][11][11][11]; // Матрица видимости граней куба
+vector <int> edgeNeighbors(255); // Соседний край индексного ребра
+unsigned int shortestPaths[255][11][11]; // Матриа с самой короткой длиной пути
+bool visibleFaces[11][11][11][11]; // Матрица видимости граней куба
 int m, n; // Размеры лабиринта
 struct point { int x, y; }; // Координаты точки в лабиринте
 
 // Проверяет является ли { sx, sy } -> { x, y } допустимым ходом
-bool valid(int sx, int sy, int x, int y)
+bool isValidMove(int sx, int sy, int x, int y)
 {
-    return x <= m && y <= n && x > 0 && y > 0 && V[sx][sy][x][y];
+    return x <= m && y <= n && x > 0 && y > 0 && visibleFaces[sx][sy][x][y];
 }
 
 // Возвращает конфигурацию, полученную путем поворота этой конфигурации на 90*deg градусов. 
 // Конфигурация определяется идентификатором левого края верхней поверхности куба
-int rotate(int dfsStack, int deg)
+int getRotatedConfiguration(int dfsStack, int deg)
 {
     return (dfsStack + deg) % 4 + (dfsStack & 240);
 }
 
 // Возвращает грань, полученную путем переворачивания этой грани вдоль заданного направления
-int flip(int dfsStack, int dir)
+int getFlippedFace(int dfsStack, int directions)
 {
-    if (dir == 0) // влево
+    if (directions == 0) // влево
     {
-        return N[rotate(dfsStack, 2)];
+        return edgeNeighbors[getRotatedConfiguration(dfsStack, 2)];
     }
-    else if (dir == 1) // вверх
+    else if (directions == 1) // вверх
     {
-        return rotate(N[rotate(dfsStack, 3)], 3);
+        return getRotatedConfiguration(edgeNeighbors[getRotatedConfiguration(dfsStack, 3)], 3);
     }
-    else if (dir == 2) // вправо
+    else if (directions == 2) // вправо
     {
-       return rotate(N[dfsStack], 2); 
+       return getRotatedConfiguration(edgeNeighbors[dfsStack], 2); 
     }
-    else if (dir == 3) // вниз
+    else if (directions == 3) // вниз
     {
-        return rotate(N[rotate(dfsStack, 1)], 1);
+        return getRotatedConfiguration(edgeNeighbors[getRotatedConfiguration(dfsStack, 1)], 1);
     }
     return 0;
 }
 
 int main()
 {
-    string str;
-    bool vertical;
+    string inputStr;
+    bool isVertical;
     int a, b, c, d, x, y;
-    memset(V, true, sizeof(V)); // эффективно заполняем массив значениями true, все грани видны изначально
-    memset(A, 255, sizeof(A)); // эффективно заполняем массив значениями 255, все расстояния изначально бесконечны
 
-    N[160] = 177; N[161] = 243; N[162] = 209; N[163] = 193; N[208] = 194; N[209] = 162;
-    N[210] = 242; N[211] = 226; N[192] = 178; N[193] = 163; N[194] = 208; N[195] = 225;
-    N[176] = 240; N[177] = 160; N[178] = 192; N[179] = 224; N[224] = 179; N[225] = 195; 
-    N[226] = 211; N[227] = 241; N[240] = 176; N[241] = 227; N[242] = 210; N[243] = 161;
+    memset(visibleFaces, true, sizeof(visibleFaces)); // эффективно заполняем массив значениями true, все грани видны изначально
+    memset(shortestPaths, 255, sizeof(shortestPaths)); // эффективно заполняем массив значениями 255, все расстояния изначально бесконечны
+
+    edgeNeighbors[160] = 177; edgeNeighbors[161] = 243; edgeNeighbors[162] = 209; edgeNeighbors[163] = 193; edgeNeighbors[208] = 194; edgeNeighbors[209] = 162;
+    edgeNeighbors[210] = 242; edgeNeighbors[211] = 226; edgeNeighbors[192] = 178; edgeNeighbors[193] = 163; edgeNeighbors[194] = 208; edgeNeighbors[195] = 225;
+    edgeNeighbors[176] = 240; edgeNeighbors[177] = 160; edgeNeighbors[178] = 192; edgeNeighbors[179] = 224; edgeNeighbors[224] = 179; edgeNeighbors[225] = 195; 
+    edgeNeighbors[226] = 211; edgeNeighbors[227] = 241; edgeNeighbors[240] = 176; edgeNeighbors[241] = 227; edgeNeighbors[242] = 210; edgeNeighbors[243] = 161;
     stack<pair<point, int>, vector<pair<point, int>>> dfsStack; // dfs stack
 
-
-
-    // Read the input
-    std::cin >> m >> n >> a >> b >> c >> d;
-    while(cin >> str)
+    cin >> m >> n >> a >> b >> c >> d;
+    while(cin >> inputStr)
     {
-        if(str[0] == 'v')
+        if(inputStr[0] == 'v')
         {
-            vertical = true;
+            isVertical = true;
         }
-        else if(str[0] == 'h')
+        else if(inputStr[0] == 'h')
         {
-            vertical = false;
+            isVertical = false;
         }
         else
         {
-            x = std::stoi(str);
-            std::cin >> y;
-            if(vertical)
+            x = stoi(inputStr);
+            cin >> y;
+            if(isVertical)
             {
-                V[x+1][y][x][y] = V[x][y][x+1][y] = false;
+                visibleFaces[x+1][y][x][y] = visibleFaces[x][y][x+1][y] = false;
             }
             else
             {
-                V[x][y][x][y+1] = V[x][y+1][x][y] = false;
+                visibleFaces[x][y][x][y+1] = visibleFaces[x][y+1][x][y] = false;
             }
         }
     }
 
     // Dfs
-    const point dir[4] = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-    dfsStack.push( { { a, b }, 160 } );
-    A[160][a][b] = 0;
+    const point directions[4] = {{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }};
+    dfsStack.push({{ a, b }, 160 });
+    shortestPaths[160][a][b] = 0;
     while(!dfsStack.empty())
     {
-        auto p = dfsStack.top().first;
-        auto side = dfsStack.top().second;
+        auto currentPosition = dfsStack.top().first;
+        auto currentConfiguration = dfsStack.top().second;
         dfsStack.pop();
         for(int i = 0; i < 4; i++)
         {
-            auto d = dir[i];
-            point pd = { p.x+d.x, p.y+d.y };
-            if(valid(p.x, p.y, pd.x, pd.y))
+            auto d = directions[i];
+            point pd = { currentPosition.x + d.x, currentPosition.y + d.y };
+            if(isValidMove(currentPosition.x, currentPosition.y, pd.x, pd.y))
             {
-                auto newside = flip(side, i);
-                if(A[newside][pd.x][pd.y] > A[side][p.x][p.y])
+                auto newConfiguration = getFlippedFace(currentConfiguration, i);
+                if(shortestPaths[newConfiguration][pd.x][pd.y] > shortestPaths[currentConfiguration][currentPosition.x][currentPosition.y])
                 {
-                    A[newside][pd.x][pd.y] = A[side][p.x][p.y] + 1;
-                    dfsStack.push( { { pd.x, pd.y }, newside } );
+                    shortestPaths[newConfiguration][pd.x][pd.y] = shortestPaths[currentConfiguration][currentPosition.x][currentPosition.y] + 1;
+                    dfsStack.push({{pd.x, pd.y}, newConfiguration });
                 }
             }
         }
     }
-    unsigned int mmin = 999999999;
+    unsigned int minPathLength = 999999999;
     for(int i = 0; i < 4; i++)
     {
-        mmin = min(mmin, A[160 + i][c][d]);
+        minPathLength = min(minPathLength, shortestPaths[160 + i][c][d]);
     }
-    if(mmin == 999999999)
+    if(minPathLength == 999999999)
+    {
         cout << "No solution";
+    }
     else
-        cout << mmin;
+    {
+        cout << minPathLength;
+    }
 }
