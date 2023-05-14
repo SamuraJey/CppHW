d5598bf4e04c5c915daf332a5d517f200b9b7b02
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-05-14 22:38:14 +0500

    awadaw

--- ClassWork 13.05.2023/task_730.cc
+++ ClassWork 13.05.2023/task_730.cc
@@ -1,12 +1,14 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-struct Edge {
+struct Edge
+{
     int u, v, w, id;
-    bool operator<(const Edge& other) const {
+    bool operator<(const Edge &other) const
+    {
         return w < other.w;
     }
 };
@@ -19,38 +21,47 @@ Edge edges[MAXM];
 int parent[MAXN];
 int rankk[MAXN];
 
-int find(int u) {
-    if (parent[u] == u) {
+int find(int u)
+{
+    if (parent[u] == u)
+    {
         return u;
     }
     return parent[u] = find(parent[u]);
 }
 
-void unite(int u, int v) {
+void unite(int u, int v)
+{
     u = find(u);
     v = find(v);
-    if (u != v) {
-        if (rankk[u] < rankk[v]) {
+    if (u != v)
+    {
+        if (rankk[u] < rankk[v])
+        {
             swap(u, v);
         }
         parent[v] = u;
-        if (rankk[u] == rankk[v]) {
+        if (rankk[u] == rankk[v])
+        {
             rankk[u]++;
         }
     }
 }
 
-int main() {
+int main()
+{
     cin >> n >> m;
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         cin >> edges[i].u >> edges[i].v >> edges[i].w;
         edges[i].id = i + 1;
     }
 
     sort(edges, edges + m);
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         parent[i] = i;
         rankk[i] = 0;
     }
@@ -59,8 +70,10 @@ int main() {
     int tree_cost = 0;
 
     // добавляем все ребра, выходящие из вершины номер 1
-    for (int i = 0; i < m; i++) {
-        if (edges[i].u == 1) {
+    for (int i = 0; i < m; i++)
+    {
+        if (edges[i].u == 1)
+        {
             tree_edges.push_back(edges[i].id);
             tree_cost += edges[i].w;
             unite(edges[i].u, edges[i].v);
@@ -68,11 +81,13 @@ int main() {
     }
 
     // продолжаем добавлять рёбра по алгоритму Крускала
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         int u = edges[i].u;
         int v = edges[i].v;
         int w = edges[i].w;
-        if (find(u) != find(v)) {
+        if (find(u) != find(v))
+        {
             unite(u, v);
             tree_edges.push_back(edges[i].id);
             tree_cost += w;
@@ -80,7 +95,8 @@ int main() {
     }
 
     cout << tree_cost << " " << tree_edges.size() << endl;
-    for (int i = 0; i < tree_edges.size(); i++) {
+    for (int i = 0; i < tree_edges.size(); i++)
+    {
         cout << tree_edges[i] << " ";
     }
     cout << endl;
