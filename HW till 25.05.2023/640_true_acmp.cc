5bf4c219a7149420333f377adae215a3fbe394cf
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-05-28 12:23:38 +0500

    lenght still 657, but more cursed

--- HW till 25.05.2023/640_true_acmp.cc
+++ HW till 25.05.2023/640_true_acmp.cc
@@ -5,7 +5,7 @@ using namespace std;
 #define D int
 #define S(a) a.size()
 #define J return
-#define F(i, c) for (D i=0; i < c ; i++)
+#define F(i, c, l) for (D i=0; i < c ; i++) {l}
 #define P a[i][j]
 
 V R()
@@ -13,9 +13,7 @@ V R()
     D p, f;
     cin >> p >> f;
     V a(p, H(f));
-    F(i, p)
-    F(j, f)
-    cin >> P;
+    F(i, p, F(j, f, cin >> P;))
     J a;
 }
 
@@ -23,10 +21,7 @@ V d(V a)
 {
     D p = S(a), f = S(a[0]);
     V r(f, H(p));
-
-    F(i, p)
-    F(j, f)
-    r[j][i] = P;
+    F(i, p, F(j, f, r[j][i] = P;))
     J r;
 }
 
@@ -34,10 +29,7 @@ V h(V a)
 {
     D p = S(a), f = S(a[0]);
     V r(p, H(f));
-
-    F(i, p)
-    F(j, f)
-    r[p - 1 - i][j] = P;
+    F(i, p, F(j, f, r[p - 1 - i][j] = P;))
     J r;
 }
 
@@ -45,12 +37,7 @@ V R(V a)
 {
     D p = S(a), f = S(a[0]), s = p, z = -1, q = f, l = -1;
 
-    F(i, p)
-    F(j, f)
-    if (P == '#')
-    {
-        s = min(s, i), q = min(q, j), z = max(z, i), l = max(l, j);
-    }
+    F(i, p, F(j, f, if (P == '#'){(s = min(s, i), q = min(q, j), z = max(z, i), l = max(l, j));}))
     if (z == -1)
         J V();
     V r(z - s + 1, H(l - q + 1));
@@ -68,18 +55,6 @@ V T(V a)
 main()
 {
     V a = R(R()), b = R(R());
-    F(i, 2)
-    {
-        a = h(a);
-        F(j, 4)
-        {
-            a = T(a);
-            if (a == b)
-            {
-                cout << "Yes";
-                J 0;
-            }
-        }
-    }
+    F(i, 2, a = h(a);F(j, 4, a = T(a);if (a == b){cout << "Yes";J 0;}))
     cout << "No";
 }
